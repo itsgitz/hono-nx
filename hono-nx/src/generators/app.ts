692995d7072a5d3cf2ac54a627a8781ed094164b
itsgitz <anggit.ginanjar.dev@gmail.com>
2025-09-28 10:22:36 +0700

    chore: added templates for hono starter

--- hono-nx/src/generators/app.ts
+++ hono-nx/src/generators/app.ts
@@ -2,20 +2,51 @@ import {
   addProjectConfiguration,
   formatFiles,
   generateFiles,
+  joinPathFragments,
+  offsetFromRoot,
   Tree,
 } from '@nx/devkit';
 import * as path from 'path';
 import { AppGeneratorSchema } from './schema';
 
 export async function appGenerator(tree: Tree, options: AppGeneratorSchema) {
-  const projectRoot = `libs/${options.name}`;
+  //
+  // By default we create apps under the `apps/` directory.
+  const projectRoot = options.directory
+    ? `${options.directory}/${options.name}`
+    : `apps/${options.name}`;
+
   addProjectConfiguration(tree, options.name, {
     root: projectRoot,
-    projectType: 'library',
-    sourceRoot: `${projectRoot}/src`,
-    targets: {},
+    projectType: 'application',
+    sourceRoot: joinPathFragments(projectRoot, 'src'),
+    targets: {
+      build: {
+        executor: 'nx:run-commands',
+        options: {
+          command: 'npm run build',
+        },
+      },
+      serve: {
+        executor: 'nx:run-commands',
+        options: {
+          command: 'npm run serve',
+        },
+      },
+      start: {
+        executor: 'nx:run-commands',
+        options: {
+          command: 'npm run start',
+        },
+      },
+    },
   });
-  generateFiles(tree, path.join(__dirname, 'files'), projectRoot, options);
+  generateFiles(tree, path.join(__dirname, 'files'), projectRoot, {
+    offsetFromRoot: offsetFromRoot(projectRoot),
+    name: options.name,
+    port: options.port ?? 3000,
+    tmpl: '',
+  });
   await formatFiles(tree);
 }
 
--- hono-nx/src/generators/schema.d.ts
+++ hono-nx/src/generators/schema.d.ts
@@ -1,3 +1,5 @@
 export interface AppGeneratorSchema {
   name: string;
+  port?: number;
+  directory: string;
 }
